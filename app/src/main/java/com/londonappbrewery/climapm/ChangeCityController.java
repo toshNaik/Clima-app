34f95fddf61d106d9a3e6891b82bd80564fbd96a
toshNaik <34796852+toshNaik@users.noreply.github.com>
2019-10-09 23:03:02 +0530

    Clima app

---
+++ app/src/main/java/com/londonappbrewery/climapm/ChangeCityController.java
@@ -0,0 +1,39 @@
+package com.londonappbrewery.climapm;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class ChangeCityController extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.change_city_layout);
+        final EditText editText = findViewById(R.id.queryET);
+        ImageButton backButton = findViewById(R.id.backButton);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                String newCity = editText.getText().toString();
+                Intent intent = new Intent(ChangeCityController.this, WeatherController.class);
+                intent.putExtra("City", newCity);
+                startActivity(intent);
+                return false;
+            }
+        });
+    }
+}
